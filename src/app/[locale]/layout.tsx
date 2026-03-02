e90288053c4d67be108840c2a8b517900b356d0b
alimeer786 <alimeer605@gmail.com>
2026-03-02 15:33:57 +0500

    gtm

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import Script from "next/script";
 import { Manrope, Playfair_Display } from "next/font/google";
 import { NextIntlClientProvider, hasLocale } from "next-intl";
 import { getMessages, setRequestLocale } from "next-intl/server";
@@ -9,9 +8,7 @@ import { FloatingContactButtons } from "@/components/FloatingContactButtons";
 import { Header } from "@/components/Header";
 import { routing } from "@/i18n/routing";
 import "../globals.css";
-
-const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID;
-
+import GTM from "@/components/GTM";
 const sans = Manrope({
   subsets: ["latin"],
   variable: "--font-sans",
@@ -60,33 +57,9 @@ export default async function LocaleLayout({
 
   return (
     <html lang={locale} className={`${sans.variable} ${serif.variable}`}>
-      <head>
-        {/* Google Tag Manager */}
-        {GTM_ID ? (
-          <Script id="gtm-loader" strategy="afterInteractive">
-            {`
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','${GTM_ID}');
-            `}
-          </Script>
-        ) : null}
-      </head>
-
       <body className="bg-[var(--color-surface)] text-[var(--color-ink)] antialiased">
-        {/* Google Tag Manager (noscript) */}
-        {GTM_ID ? (
-          <noscript>
-            <iframe
-              src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
-              height="0"
-              width="0"
-              style={{ display: "none", visibility: "hidden" }}
-            />
-          </noscript>
-        ) : null}
+        {/* Google Tag Manager */}
+        <GTM />
 
         <NextIntlClientProvider locale={locale} messages={messages}>
           <div className="flex min-h-screen flex-col">
