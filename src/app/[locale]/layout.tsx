15fcede9015afbc615f98b89dd14a9013cbd4b24
alimeer786 <alimeer605@gmail.com>
2026-03-02 14:56:21 +0500

    gtm

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -7,8 +7,8 @@ import { Footer } from "@/components/Footer";
 import { FloatingContactButtons } from "@/components/FloatingContactButtons";
 import { Header } from "@/components/Header";
 import { routing } from "@/i18n/routing";
+import { GoogleTagManager } from "@next/third-parties/google";
 import "../globals.css";
-import Script from "next/script";
 
 const sans = Manrope({
   subsets: ["latin"],
@@ -58,32 +58,9 @@ export default async function LocaleLayout({
 
   return (
     <html lang={locale} className={`${sans.variable} ${serif.variable}`}>
-      <head>
-        <Script
-          id="gtm-script"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id=${process.env.NEXT_PUBLIC_GTM_ID}'+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_ID}');
-          `,
-          }}
-        />
-      </head>
+      <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM_ID!} />
 
       <body className="bg-[var(--color-surface)] text-[var(--color-ink)] antialiased">
-        <noscript>
-          <iframe
-            src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_ID}`}
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          />
-        </noscript>
-
         <NextIntlClientProvider locale={locale} messages={messages}>
           <div className="flex min-h-screen flex-col">
             <Header />
